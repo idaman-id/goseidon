e2559ec3af238eaafbf00119f53a852c0368f2d7
KokoRaka <raka.suryadi@gmail.com>
2021-10-10 17:10:13 +0700

    Change app init

--- cmd/web-service/main.go
+++ cmd/web-service/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"idaman.id/storage/pkg/config"
 	"idaman.id/storage/pkg/rest"
 )
 
 func main() {
 	app := rest.CreateApp()
-	app.Listen(":3000")
+	address := config.GetString("APP_HOST") + ":" + config.GetString("APP_PORT")
+	app.Listen(address)
 }
